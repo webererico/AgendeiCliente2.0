c541dd10c29056dbd9ee15d9e7f04aad3dbca5ca
Erico Weber <ericoweber@hotmail.com>
2020-04-12 22:06:34 -0300

    - pagina login - botao de sair no final da pagina - tela editar perfil - tela serviços por categoria (nao carrega a primeira vez) - tela login quando surge teclado - custon drawer mudando conforme estado do login - tela de agendamento exibir os funcionários e a seguir escolha de horário !!!!! - genero no login e no profile funcionando - login pelo google (criacao de conta também) - verificacao e-mail ja existe e senha - apagar estabelecimento favorito - implementar o buscar (lupa) - Não vai para a pagina da empresa - metodo de pagamento falta poder apagar o metodo - utilizando o timestamp da maneira certa - tela inicial do app
    ERROS:
    - deletar evento agendado sem verificar
    - verificar se o horário ja esta agendado no scheduleScreen
    - alterar horario de agendamento
    - botao favorito
    - drawer com problema (Nao mostra o nome) quando loga pelo google
    - aterar agendamento erro na mudanca de horario
    
    FALTA:
    - terminar o metodo de login pela apple

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:scoped_model/scoped_model.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
